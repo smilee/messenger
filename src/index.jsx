754ebfe30e6412a3078de122917d943a617d2f26
Seungmin Lee <seungmin@uxplus.dev>
2020-09-29 02:11:57 +0900

    Initialize project

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <p>Hello, world!</p>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
